2a1156c617577175ea64b5c89e8e1f6555fe3c18
jocunda <silviawanshan88@gmail.com>
2021-12-02 19:49:42 +0800

    css edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  MdPlaylistAdd,
+} from 'react-icons/all';
+import CardList from "./components/CardList"
 
-function App() {
-  return (
+export default function App() {
+
+  return <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To do List</h1>
+      <div className="input-add">
+        <input type="text"
+          name="todoadd"
+          id="inputtodo"
+          placeholder="Add..." />
+        <MdPlaylistAdd />
+      </div>
+      <CardList />
     </div>
-  );
-}
-
-export default App;
+  </>
+};
---
+++ src/components/CardList.tsx
@@ -0,0 +1,18 @@
+import "./Component.css"
+import {
+    MdOutlineDeleteOutline,
+    ImCheckboxUnchecked,
+    ImCheckboxChecked
+} from 'react-icons/all';
+
+export default function CardList() {
+
+    return <>
+        <div className="card">
+            <ImCheckboxUnchecked id="icon-left" className="float" />
+            <ImCheckboxChecked className="float" />
+            <span id="center" className="float">Hello</span>
+            <MdOutlineDeleteOutline id="icon-right" className="float" />
+        </div>
+    </>
+};
